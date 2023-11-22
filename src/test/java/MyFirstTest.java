2db435c6543f7aa1193c7b6f5c25c61b8ee36760
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-11-22 22:27:27 +0200

    Add interface and endpoints

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,16 +1,25 @@
 import config.VideoGameConfig;
+import config.VideoGameEndpoints;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 
 public class MyFirstTest extends VideoGameConfig {
+
     @Test
-    public void MyFirstTest() {
+    public void myFirstTest() {
         given()
                 .log().all()
                 .when()
-                .get("videogame")
+                .get("/videogame")
                 .then()
                 .log().all();
+
+    }
+
+    @Test
+    public void myFirstTestWithEndpoint() {
+        get(VideoGameEndpoints.ALL_VIDEO_GAMES)
+                .then().log().all();
     }
 }
