f6214de1361a5784d126c690ddaa08e47273dab8
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-11-23 22:11:56 +0200

    create football api config file

---
+++ src/test/java/config/FootballConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.BeforeClass;
+
+public class FootballConfig {
+
+    @BeforeClass
+    public static void setup(){
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://api.football-data.org")
+                .setBasePath("/v4")
+                .addHeader("X-Auth-Token", FootballToken.token) // The file has been excluded. Recreate it!
+                .addHeader("X-Response-Control", "minified")
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+}
