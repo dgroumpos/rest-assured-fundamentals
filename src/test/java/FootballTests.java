b966b60c28b86c870d6f4545506d5e6b82ae1394
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-11-24 22:21:46 +0200

    http response assertion test

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -2,10 +2,11 @@ import config.FootballConfig;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class FootballTests extends FootballConfig {
     @Test
-    public void getDetailsOfOneArea(){
+    public void getDetailsOfOneArea() {
         given()
                 .queryParam("areas", 2076)
                 .when()
@@ -14,12 +15,30 @@ public class FootballTests extends FootballConfig {
     }
 
     @Test
-    public void getDetailsOfMultipleAreas(){
+    public void getDetailsOfMultipleAreas() {
         String areaIds = "2076,2077,2080";
 
         given()
                 .urlEncodingEnabled(false)
                 .queryParam("areas", areaIds)
                 .get("/areas");
+    }
+
+    @Test
+    public void getDateFounded() {
+        given()
+                .when()
+                .get("teams/57")
+                .then()
+                .body("founded", equalTo(1886));
+    }
+
+    @Test
+    public void getFirstTeamName(){
+        given()
+                .when()
+                .get("competitions/2021/teams")
+                .then()
+                .body("teams.name[0]", equalTo("Arsenal FC"));
     }
 }
