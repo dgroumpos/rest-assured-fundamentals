3adb98aa784bc1ca10ff3339509d02c7f04129b3
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-11-25 16:04:32 +0200

    gpath JSON findAll to extract multiple data

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -3,6 +3,7 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -15,5 +16,19 @@ public class GPathJSONTests extends FootballConfig {
         Map<String, ?> allTeamDataForSingleTeam = response.path("teams.find {it.name == 'Manchester United FC'}");
 
         System.out.println("Map of team data = " + allTeamDataForSingleTeam);
+    }
+
+    @Test
+    public void extractSingleValueWithFind(){
+        Response response = get("teams/57");
+        String specificPlayer = response.path("squad.find {it.id == 4832}.name");
+        System.out.println("Name of the player: " + specificPlayer);
+    }
+
+    @Test
+    public void extractListOfValuesWithFindAll(){
+        Response response = get("teams/57");
+        List<String> playerNames = response.path("squad.findAll {it.id >= 6154}");
+        System.out.println("List of players = " + playerNames);
     }
 }
